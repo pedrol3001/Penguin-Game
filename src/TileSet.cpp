6eeb3d19ba0737cdc93837ea1449840fd2b41106
Pedro Luis <pedrolcr@outlook.com>
2022-07-04 10:34:21 -0300

    tileset

---
+++ src/TileSet.cpp
@@ -0,0 +1,21 @@
+#include "TileSet.h"
+
+TileSet::TileSet(int tileWidth, int tileHeight, string file) : tileSet(file), tileWidth(tileWidth), tileHeight(tileHeight) {
+  rows = tileSet.GetHeight() / tileHeight;
+  columns = tileSet.GetWidth() / tileWidth;
+}
+
+void TileSet::RenderTile(unsigned index, float x, float y) {
+  if(static_cast<int>(index) >= rows * columns -1) return;
+
+  tileSet.SetClip((index % columns) * tileWidth, (index / columns) * tileHeight, tileWidth, tileHeight);
+  tileSet.Render(x, y);
+}
+
+int TileSet::GetTileWidth() {
+  return tileWidth;
+}
+
+int TileSet::GetTileHeight() {
+  return tileHeight;
+}
